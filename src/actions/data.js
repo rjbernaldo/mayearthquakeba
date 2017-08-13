d1a45874e015ddb673fc06b7812f78994c26c1e1
Rj Bernaldo <rj.bernaldo@gmail.com>
2017-08-13 19:30:34 +0800

    Add usgs query url

--- src/actions/data.js
+++ src/actions/data.js
@@ -1,6 +1,8 @@
 export const FETCH_DATA = 'FETCH_DATA';
 export const UPDATE_DATA = 'UPDATE_DATA';
 
+const URL = 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&latitude=11.6899014&longitude=122.3708869&maxradiuskm=1150&limit=10&orderby=time';
+
 export function fetchData(data) {
   return {
     type: FETCH_DATA,
