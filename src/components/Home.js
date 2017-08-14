a2ad5ed474248800d467c088f9f09059a51ddbe5
Rj Bernaldo <rj.bernaldo@gmail.com>
2017-08-14 20:38:17 +0800

    Fix syntax problems

--- src/components/Home.js
+++ src/components/Home.js
@@ -107,11 +107,11 @@ function formatTime(date) {
   let hours = date.getHours();
   let minutes = date.getMinutes();
   const ampm = hours >= 12 ? 'PM' : 'AM';
-  hours = hours % 12;
-  hours = hours ? hours : 12; // the hour '0' should be '12'
-  minutes = minutes < 10 ? '0'+minutes : minutes;
-  const strTime = hours + ':' + minutes + ' ' + ampm;
-  return strTime;
+  hours %= 12;
+  if (!hours) hours = 12;
+  minutes = minutes < 10 ? `0${minutes}` : minutes;
+
+  return `${hours}:${minutes} ${ampm}`;
 }
 
 function timeSince(date) {
